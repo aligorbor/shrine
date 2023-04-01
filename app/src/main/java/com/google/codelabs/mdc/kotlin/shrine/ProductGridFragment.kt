5ecbfeb691ca8724ee7c465ad4c0dca81ca4bb1b
aligorbor <aligorbor@list.ru>
2023-04-01 23:34:14 +0500

    Added home screen

--- app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/ProductGridFragment.kt
+++ app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/ProductGridFragment.kt
@@ -1,16 +1,46 @@
 package com.google.codelabs.mdc.kotlin.shrine
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.codelabs.mdc.kotlin.shrine.databinding.ShrProductGridFragmentBinding
+import com.google.codelabs.mdc.kotlin.shrine.network.ProductEntry
 
 class ProductGridFragment : Fragment() {
+    private lateinit var binding: ShrProductGridFragmentBinding
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.shr_product_grid_fragment, container, false)
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = ShrProductGridFragmentBinding.inflate(inflater, container, false)
+        (activity as AppCompatActivity).setSupportActionBar(binding.appBar)
+        with(binding) {
+            recyclerView.setHasFixedSize(true)
+            recyclerView.layoutManager = GridLayoutManager(context, 2, RecyclerView.VERTICAL, false)
+            val adapter =
+                ProductCardRecyclerViewAdapter(ProductEntry.initProductEntryList(resources))
+            recyclerView.adapter = adapter
+            val largePadding = resources.getDimensionPixelSize(R.dimen.shr_product_grid_spacing)
+            val smallPadding = resources.getDimensionPixelSize(R.dimen.shr_product_grid_spacing_small)
+            recyclerView.addItemDecoration(ProductGridItemDecoration(largePadding, smallPadding))
+        }
+
+
+        return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.shr_toolbar_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
     }
 }
--- app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/network/ProductEntry.kt
+++ app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/network/ProductEntry.kt
@@ -12,7 +12,12 @@ import java.util.*
  * A product entry in the list of products.
  */
 class ProductEntry(
-        val title: String, dynamicUrl: String, val url: String, val price: String, val description: String) {
+    val title: String,
+    dynamicUrl: String,
+    val url: String,
+    val price: String,
+    val description: String
+) {
     val dynamicUrl: Uri = Uri.parse(dynamicUrl)
 
     companion object {
