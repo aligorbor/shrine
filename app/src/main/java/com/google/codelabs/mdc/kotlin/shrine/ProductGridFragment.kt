480cc5f3a23679e2a1b2e7cf9c7fdb68b4bd5666
aligorbor <aligorbor@list.ru>
2023-04-08 23:19:57 +0500

    Adjusted elevation and used a staggered RecyclerView adapter

--- app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/ProductGridFragment.kt
+++ app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/ProductGridFragment.kt
@@ -8,6 +8,7 @@ import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.codelabs.mdc.kotlin.shrine.databinding.ShrProductGridFragmentBinding
 import com.google.codelabs.mdc.kotlin.shrine.network.ProductEntry
+import com.google.codelabs.mdc.kotlin.shrine.staggeredgridlayout.StaggeredProductCardRecyclerViewAdapter
 
 class ProductGridFragment : Fragment() {
     private lateinit var binding: ShrProductGridFragmentBinding
@@ -20,13 +21,29 @@ class ProductGridFragment : Fragment() {
         binding = ShrProductGridFragmentBinding.inflate(inflater, container, false)
         (activity as AppCompatActivity).setSupportActionBar(binding.appBar)
         with(binding) {
+//            recyclerView.setHasFixedSize(true)
+//            recyclerView.layoutManager = GridLayoutManager(context, 2, RecyclerView.VERTICAL, false)
+//            val adapter =
+//                ProductCardRecyclerViewAdapter(ProductEntry.initProductEntryList(resources))
+//            recyclerView.adapter = adapter
+//            val largePadding = resources.getDimensionPixelSize(R.dimen.shr_product_grid_spacing)
+//            val smallPadding = resources.getDimensionPixelSize(R.dimen.shr_product_grid_spacing_small)
+//            recyclerView.addItemDecoration(ProductGridItemDecoration(largePadding, smallPadding))
+
+            // Set up the RecyclerView
             recyclerView.setHasFixedSize(true)
-            recyclerView.layoutManager = GridLayoutManager(context, 2, RecyclerView.VERTICAL, false)
-            val adapter =
-                ProductCardRecyclerViewAdapter(ProductEntry.initProductEntryList(resources))
+            val gridLayoutManager = GridLayoutManager(context, 2, RecyclerView.HORIZONTAL, false)
+            gridLayoutManager.spanSizeLookup = object : GridLayoutManager.SpanSizeLookup() {
+                override fun getSpanSize(position: Int): Int {
+                    return if (position % 3 == 2) 2 else 1
+                }
+            }
+            recyclerView.layoutManager = gridLayoutManager
+            val adapter = StaggeredProductCardRecyclerViewAdapter(
+                ProductEntry.initProductEntryList(resources))
             recyclerView.adapter = adapter
-            val largePadding = resources.getDimensionPixelSize(R.dimen.shr_product_grid_spacing)
-            val smallPadding = resources.getDimensionPixelSize(R.dimen.shr_product_grid_spacing_small)
+            val largePadding = resources.getDimensionPixelSize(R.dimen.shr_staggered_product_grid_spacing_large)
+            val smallPadding = resources.getDimensionPixelSize(R.dimen.shr_staggered_product_grid_spacing_small)
             recyclerView.addItemDecoration(ProductGridItemDecoration(largePadding, smallPadding))
         }
 
